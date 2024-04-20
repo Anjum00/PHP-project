88e4fdf12e6d3abc34e5a97805efccf2d45d3f74
Anjum00 <105652255+Anjum00@users.noreply.github.com>
2024-04-20 13:17:33 +0530

    Add files via upload

---
+++ bbdms.sql
@@ -0,0 +1,301 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 10, 2024 at 07:22 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bbdms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(10) NOT NULL,
+  `AdminName` varchar(120) DEFAULT NULL,
+  `UserName` varchar(120) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(1, 'Admin', 'admin', 8979555558, 'admin@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2023-12-31 23:06:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblblooddonars`
+--
+
+CREATE TABLE `tblblooddonars` (
+  `id` int(11) NOT NULL,
+  `FullName` varchar(100) DEFAULT NULL,
+  `MobileNumber` char(11) DEFAULT NULL,
+  `EmailId` varchar(100) DEFAULT NULL,
+  `Gender` varchar(20) DEFAULT NULL,
+  `Age` int(11) DEFAULT NULL,
+  `BloodGroup` varchar(20) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `userrole` text DEFAULT NULL,
+  `PostingDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` int(1) DEFAULT NULL,
+  `Password` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblblooddonars`
+--
+
+INSERT INTO `tblblooddonars` (`id`, `FullName`, `MobileNumber`, `EmailId`, `Gender`, `Age`, `BloodGroup`, `Address`, `userrole`, `PostingDate`, `status`, `Password`) VALUES
+(6, 'Ashu Misra', '7797987981', 'ashu@gmail.com', 'Male', 35, 'O-', 'H-900, Ravinderpuri colony, Varanasi', ' Call me if blood require', '2024-01-02 07:13:41', 0, '202cb962ac59075b964b07152d234b70'),
+(9, 'Test Demo', '9789797979', 'test@gmail.com', 'Male', 56, 'A+', 'Allahabad', ' gjhgjhgjhghjghj', '2024-01-03 00:39:08', 1, '202cb962ac59075b964b07152d234b70'),
+(10, 'John Doe', '1236547890', 'johnd@gmail.com', 'Male', 25, 'O-', 'A 5623 XYZ Street New Delhi', ' NA', '2024-01-02 20:20:58', 1, 'f925916e2754e5e03f75dd58a5733251'),
+(11, 'Amit Kumar', '1231231230', 'amitk@gmail.com', 'Male', 26, 'AB+', 'ABc Street Sector 20 Noida UP', ' NA', '2024-01-03 19:52:52', 1, 'f925916e2754e5e03f75dd58a5733251'),
+(12, 'Anuj kumar', '1425362514', 'ak@test.com', 'Male', 30, 'A-', 'NA', ' NA', '2024-01-05 12:01:08', 1, 'f925916e2754e5e03f75dd58a5733251'),
+(13, 'Anjum saiyad', '9316380789', 'anjumsaiyd085@gmail.com', 'Male', 20, 'A-', 'Bhavnagar,Gujrat,India', ' nopes', '2024-04-05 11:36:08', 1, '8e4928086a47fa3e12281b1c959aea91'),
+(14, 'Demo Acc', '9316380789', 'demo@gmail.com', 'Male', 20, 'AB-', 'Bhavnagar,Gujrat,India', NULL, '2024-04-06 06:24:14', 1, '4db71159daedc2d0790c3c3d5c603276'),
+(15, 'John', '9898986532', 'john@lorem.com', 'Male', 28, 'O-', '101,AAKASH GANGA', 'bloodreceiver', '2024-04-08 05:37:10', 1, '21232f297a57a5a743894a0e4a801fc3'),
+(16, 'Uvesh', '9316380789', 'uvesh@lorem.com', '', 0, 'A-', 'lorem', 'hospital', '2024-04-08 06:50:07', 1, '21232f297a57a5a743894a0e4a801fc3'),
+(17, 'Hospital', '3916588445', 'hospital@gmail.com', '', 0, 'A-', 'hospital', 'hospital', '2024-04-08 06:56:39', 1, '21232f297a57a5a743894a0e4a801fc3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblbloodgroup`
+--
+
+CREATE TABLE `tblbloodgroup` (
+  `id` int(11) NOT NULL,
+  `BloodGroup` varchar(20) DEFAULT NULL,
+  `PostingDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblbloodgroup`
+--
+
+INSERT INTO `tblbloodgroup` (`id`, `BloodGroup`, `PostingDate`) VALUES
+(1, 'A-', '2024-01-01 15:03:50'),
+(2, 'AB-', '2024-01-01 15:03:50'),
+(3, 'O-', '2024-01-01 15:03:50'),
+(4, 'A-', '2024-01-01 15:03:50'),
+(5, 'A+', '2024-01-01 15:03:50'),
+(6, 'AB+', '2024-01-01 15:03:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblbloodrequirer`
+--
+
+CREATE TABLE `tblbloodrequirer` (
+  `ID` int(10) NOT NULL,
+  `BloodDonarID` int(10) DEFAULT NULL,
+  `name` varchar(250) DEFAULT NULL,
+  `EmailId` varchar(250) DEFAULT NULL,
+  `ContactNumber` bigint(10) DEFAULT NULL,
+  `BloodRequirefor` varchar(250) DEFAULT NULL,
+  `Message` mediumtext DEFAULT NULL,
+  `ApplyDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblbloodrequirer`
+--
+
+INSERT INTO `tblbloodrequirer` (`ID`, `BloodDonarID`, `name`, `EmailId`, `ContactNumber`, `BloodRequirefor`, `Message`, `ApplyDate`) VALUES
+(1, 6, 'CC', 'rak@gmail.com', 7894561236, 'Father', 'Please help', '2024-01-06 06:27:24'),
+(2, 3, 'Mukesh', 'muk@gmail.com', 5896231478, 'Others', 'Please help', '2024-01-06 06:28:48'),
+(3, 6, 'Hitesh', 'hit@gmail.com', 1236547896, 'Brother', 'do the needful', '2024-01-07 06:32:12'),
+(4, 10, 'Rahul Singh', 'rahk@gmail.com', 2536251425, 'Mother', 'Please help me', '2024-01-07 20:21:52'),
+(5, 11, 'Anuj Kumar', 'ak@gmail.com', 8525232102, 'Others', 'Need blood on urgent basis', '2024-01-07 19:54:18'),
+(6, 12, 'Anjum saiyad', 'Bhavnagar@gmail.com', 9316380789, 'Brother', 'PLease Give me blood contact me', '2024-04-05 11:34:42'),
+(7, 0, 'Uvesh', 'uvesh@gmail.com', 9316380789, 'Father', 'PLease Give me Blood ', '2024-04-06 09:22:45');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcontactusinfo`
+--
+
+CREATE TABLE `tblcontactusinfo` (
+  `id` int(11) NOT NULL,
+  `Address` tinytext DEFAULT NULL,
+  `EmailId` varchar(255) DEFAULT NULL,
+  `ContactNo` char(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblcontactusinfo`
+--
+
+INSERT INTO `tblcontactusinfo` (`id`, `Address`, `EmailId`, `ContactNo`) VALUES
+(1, 'Test Demo test demo																									', 'test@test.com', '9316380789');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcontactusquery`
+--
+
+CREATE TABLE `tblcontactusquery` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `EmailId` varchar(120) DEFAULT NULL,
+  `ContactNumber` char(11) DEFAULT NULL,
+  `Message` longtext DEFAULT NULL,
+  `PostingDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblcontactusquery`
+--
+
+INSERT INTO `tblcontactusquery` (`id`, `name`, `EmailId`, `ContactNumber`, `Message`, `PostingDate`, `status`) VALUES
+(8, 'Jatin', 'jatin@lorem.com', '8989898989', 'Help me\r\n', '2024-04-08 08:26:33', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpages`
+--
+
+CREATE TABLE `tblpages` (
+  `id` int(11) NOT NULL,
+  `PageName` varchar(255) DEFAULT NULL,
+  `type` varchar(255) NOT NULL DEFAULT '',
+  `detail` longtext NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblpages`
+--
+
+INSERT INTO `tblpages` (`id`, `PageName`, `type`, `detail`) VALUES
+(2, 'Why Become Donor', 'donor', '<span style=\"color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; font-size: 14px; text-align: justify;\">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat</span>'),
+(3, 'About Us ', 'aboutus', '<div style=\"text-align: justify;\"><span style=\"font-size: 1em; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif;\">Welcome to the blood bank donor management system.</span></div>');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblblooddonars`
+--
+ALTER TABLE `tblblooddonars`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `bgroup` (`BloodGroup`);
+
+--
+-- Indexes for table `tblbloodgroup`
+--
+ALTER TABLE `tblbloodgroup`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `BloodGroup` (`BloodGroup`),
+  ADD KEY `BloodGroup_2` (`BloodGroup`);
+
+--
+-- Indexes for table `tblbloodrequirer`
+--
+ALTER TABLE `tblbloodrequirer`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `donorid` (`BloodDonarID`);
+
+--
+-- Indexes for table `tblcontactusinfo`
+--
+ALTER TABLE `tblcontactusinfo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblcontactusquery`
+--
+ALTER TABLE `tblcontactusquery`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblpages`
+--
+ALTER TABLE `tblpages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblblooddonars`
+--
+ALTER TABLE `tblblooddonars`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `tblbloodgroup`
+--
+ALTER TABLE `tblbloodgroup`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tblbloodrequirer`
+--
+ALTER TABLE `tblbloodrequirer`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tblcontactusinfo`
+--
+ALTER TABLE `tblcontactusinfo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblcontactusquery`
+--
+ALTER TABLE `tblcontactusquery`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `tblpages`
+--
+ALTER TABLE `tblpages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
